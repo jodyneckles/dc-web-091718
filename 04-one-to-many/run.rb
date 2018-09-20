31eab25fb8ebcae073f21e0b6b2d3314b3e0ded3
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-09-20 11:48:48 -0400

    inititalized Tweet class

--- 04-one-to-many/run.rb
+++ 04-one-to-many/run.rb
@@ -4,3 +4,10 @@ require_relative "./user.rb"
 
 coffee_dad = User.new("Coffee Dad")
 puts coffee_dad.username
+
+tweet1 = Tweet.new("I need coffee", coffee_dad)
+tweet2 = Tweet.new("I love coffee", coffee_dad)
+tweet3 = Tweet.new("Life is a meaningless void", coffee_dad)
+
+puts tweet2.user 
+puts tweet2.message
--- 04-one-to-many/tweet.rb
+++ 04-one-to-many/tweet.rb
@@ -1,4 +1,9 @@
 class Tweet
 
+    attr_reader :message, :user
+    
+    def initialize(message, user)
+        @message, @user = message, user
+    end
 
 end
