4ca6735697b0f56593abd2636293e3888a059ef3
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-09-20 11:04:11 -0400

    prelecture 04

--- 03-intro-to-oop/app.rb
+++ 03-intro-to-oop/app.rb
@@ -1,21 +1,24 @@
-# ella = {name:  "Ella", floofiness: 8}
-
-# puts ella[:name]
-
-# ella.say_hello # "Hi I'm Ella"
 class Cat
 
     # attr_reader :color
     # attr_writer :color
     attr_accessor :color, :fluffiness
-    attr_reader :name, :number_of_legs
+    attr_reader :number_of_legs
+
+    @@all = []
 
     def initialize(name, fluffiness, number_of_legs=4)
         @name = name
         @fluffiness = fluffiness
         @number_of_legs = number_of_legs
+        @@all << self
+        # when self is inside a method, "self" refers to the instance of the object
     end
 
+    def self.all
+        # when self is in _method name_ self refers to the class
+        @@all
+    end
     # def name=(cat_name) # setter
     #     @name = cat_name
     # end
@@ -24,8 +27,29 @@ class Cat
     #     @name
     # end
 
-    def say_hello
+    def name
+        puts "Hi my name is #{@name}"
+    end
+    
+    def self.get_all_names_of_cats # class method (because of 'self')
+        @@all.each do |cat|
+            puts cat.name
+        end
+    end
+
+    def say_hello # instance method (no 'self' in method name)
         "Hi my name is #{@name}"
+    end
+
+    def print_all_attributes
+        
+        puts self.name # calls getter method
+        puts @name # directly gets instance variable
+
+        # puts self.fluffiness
+        # puts self.number_of_legs
+
+
     end
 
     # def set_name_anyway=(new_name)
@@ -53,22 +77,11 @@ class Cat
 end
 
 ella = Cat.new("Ella", 10)
-puts ella.number_of_legs
-
 lexi = Cat.new("Lexi", 4, 3)
-puts lexi.number_of_legs
-# ella.name = "Ella" # ella.send(:name=, "Ella")
-# ella.fluffiness = 10
-# puts "Ella is #{ella.color}"
-# ella.color = "black"
-# puts ella.color
+# puts ella.all == lexi.all
+garfield = Cat.new("Garfield", 3)
 
+# print garfield.all
+# Cat.get_all_names_of_cats
 
-# puts ella.name
-# ella.set_name_anyway=("Heathcliff")
-# puts ella.name
-# ella.name = "garfield"
-# lexi = Cat.new("Lexi", 4)
-# lexi.name = "Lexi"
-# lexi.fluffiness = 4
-# puts ella.say_hello
+ella.print_all_attributes
