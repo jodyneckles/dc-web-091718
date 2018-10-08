1d2353f6aa4936807e18bbd229f9b5ab220dc935
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-10-08 11:41:18 -0400

    post lecture

--- 13-intro-to-sinatra-mvc/README.md
+++ 13-intro-to-sinatra-mvc/README.md
@@ -2,6 +2,10 @@
 - Explain how and why we use the MVC pattern (Model-View-Controller)
 - Map CRUD actions to RESTful routes
 
+- Models :  Where we store the models of our app (Fat models)
+- Controllers : Handle of the logic of the app (Skinny controllers)
+- Views : Templates / Presentation of the app (HTML and stuff)
+
 ## Deliverables
 
 - View a listing of all books
--- 13-intro-to-sinatra-mvc/app/controllers/application_controller.rb
+++ 13-intro-to-sinatra-mvc/app/controllers/application_controller.rb
@@ -1,3 +1,53 @@
 class ApplicationController < Sinatra::Base
 
+    set :views, "app/views"
+    set :method_override, true
+
+    get "/" do
+        erb :home
+    end
+
+    get "/books" do 
+        @books = Book.all
+        erb :index
+    end
+
+    get "/books/new" do 
+        erb :new
+    end
+    
+    get "/books/:id" do
+        id = params[:id]
+        @book = Book.find(id)
+        erb :show
+    end
+
+    post "/books" do
+        title = params["title"]
+        author = params["author"]
+        snippet = params["snippet"]
+        book = Book.create(title: title, author: author, snippet: snippet)
+        redirect "books/#{book.id}"
+    end
+
+    get "/books/:id/edit" do
+        @book = Book.find(params[:id])
+        erb :edit
+    end
+
+    patch "/books/:id" do 
+        book = Book.find(params[:id])
+        title = params["title"]
+        author = params["author"]
+        snippet = params["snippet"]
+        book.update(title: title, author: author, snippet: snippet)
+        redirect "books/#{book.id}"
+    end
+
+    delete "/books/:id" do
+        book = Book.find(params[:id])
+        book.destroy
+        redirect "books"
+    end
+
 end
