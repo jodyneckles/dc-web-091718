abfeba9d8a3db2d65dfac6fd8e5b13c7a1739421
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-10-18 10:02:31 -0400

    prelecture

---
+++ 20-cookies/snack-tracker/app/controllers/snacks_controller.rb
@@ -0,0 +1,50 @@
+class SnacksController < ApplicationController
+
+    before_action :find_snack, only: [:edit, :show, :update, :destroy]
+
+  def welcome
+  end
+
+  def index
+    @snacks = Snack.all
+  end
+
+  def show
+  end
+
+  def new
+    @snack = Snack.new
+  end
+
+  def create
+    @snack = Snack.create(snack_params)
+    if @snack.errors.any?
+      # render :new
+    else
+      redirect_to snack_path(@snack)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @snack.update(snack_params)
+    redirect_to @snack
+  end
+
+  def destroy
+    @snack.destroy
+    redirect_to snacks_path
+  end
+
+  private
+
+  def snack_params
+    params.require(:snack).permit(:name, :deliciousness, :calories, :retailer_id)
+  end
+
+  def find_snack
+    @snack = Snack.find(params[:id])
+  end
+end
